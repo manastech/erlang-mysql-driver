84274b52f5eaf414ae10cd0149445020f3eec4df
Gustavo Giráldez <ggiraldez@manas.com.ar>
2013-09-18 16:43:59 -0300

    Replace deprecated crypto functions

--- src/mysql_auth.erl
+++ src/mysql_auth.erl
@@ -175,11 +175,11 @@ bxor_binary(B1, B2) ->
 password_new([], _Salt) ->
     <<>>;
 password_new(Password, Salt) ->
-    Stage1 = crypto:sha(Password),
-    Stage2 = crypto:sha(Stage1),
-    Res = crypto:sha_final(
-	    crypto:sha_update(
-	      crypto:sha_update(crypto:sha_init(), Salt),
+    Stage1 = crypto:hash(Password, sha),
+    Stage2 = crypto:hash(Stage1, sha),
+    Res = crypto:hash_final(
+	    crypto:hash_update(
+	      crypto:hash_update(crypto:hash_init(sha), Salt),
 	      Stage2)
 	   ),
     bxor_binary(Res, Stage1).
